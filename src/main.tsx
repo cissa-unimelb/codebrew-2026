929320d9b42dba4220a483fc8195ef6c3ab99da0
Ben Alvaro <79905066+Macapps13@users.noreply.github.com>
2026-02-04 07:23:24 +1100

    fix: update main.tsx to render directly without App component and remove main2.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,14 @@ import { createRoot } from 'react-dom/client'
 
 import './globals.css';
 
-import App from './app'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <body className='bg-primary text-white flex flex-col gap-4 items-center justify-center min-h-screen'>
+        <p className="font-megatrans">BEEEEEEEER</p>
+        <p className="font-guardian-angle">BEEEEEEEER</p>
+        <p className="font-space-grotesk">BEEEEEEEER</p>
+    </body>
   </StrictMode>,
 )
