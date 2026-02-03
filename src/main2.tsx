e1d8dcf0ec53fc385ef71be1a88dc6ece622b018
Ben Alvaro <79905066+Macapps13@users.noreply.github.com>
2026-02-03 21:40:12 +1100

    chore: imported fonts

---
+++ src/main2.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import './globals.css';
+
+
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <p className="font-megatrans">BEEEEEEEER</p>
+    <p className="font-guardian-angle">BEEEEEEEER</p>
+    <p className="font-space-grotesk">BEEEEEEEER</p>
+  </StrictMode>,
+)
