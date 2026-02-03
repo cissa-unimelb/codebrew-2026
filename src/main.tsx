197e914e9bc6740b9ad5d578fec2478b42256bd1
Ben Alvaro <79905066+Macapps13@users.noreply.github.com>
2026-02-04 08:08:12 +1100

    fix: buncha vite stuff

--- src/main.tsx
+++ src/main.tsx
@@ -7,10 +7,10 @@ import './globals.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <body className='bg-primary text-white flex flex-col gap-4 items-center justify-center min-h-screen'>
-        <p className="font-megatrans">BEEEEEEEER</p>
-        <p className="font-guardian-angle">BEEEEEEEER</p>
-        <p className="font-space-grotesk">BEEEEEEEER</p>
-    </body>
-  </StrictMode>,
+    <main className="flex flex-col gap-4 items-center justify-center min-h-screen">
+      <p className="font-megatrans">BEEEEEEEER</p>
+      <p className="font-guardian-angle">BEEEEEEEER</p>
+      <p className="font-space-grotesk">BEEEEEEEER</p>
+    </main>
+  </StrictMode>
 )
